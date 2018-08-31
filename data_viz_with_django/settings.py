d2cc8b66221ecbf30e9a1aa8af17810267b5a94a
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2018-08-31 20:52:45 +0100

    remove django_nv3d as it is not used

--- data_viz_with_django/settings.py
+++ data_viz_with_django/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'viz',
     'bootstrap4',
-    'django_nvd3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
